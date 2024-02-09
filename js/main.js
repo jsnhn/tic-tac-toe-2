92684795117b99845e5f676e3a2524c12d1036de
jsnhn <jayhh122@gmail.com>
2024-02-08 23:49:58 -0800

    css refactor and started js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,56 @@
-  /*----- constants -----*/
+/*----- constants -----*/
 
 
-  /*----- state variables -----*/
+/*----- state variables -----*/
+let board, winner, score, currentPlayer
 
 
-  /*----- cached elements  -----*/
+/*----- cached elements  -----*/
+const boardEl = document.getElementById('board-container')
+const scoreEl = document.getElementById('score-container')
+const messageEl = document.querySelector('h2')
 
+/*----- event listeners -----*/
 
-  /*----- event listeners -----*/
+boardEl.addEventListener('click', handleBoardClick)
+document.querySelector('button').addEventListener('click', handleResetClick)
 
+/*----- functions -----*/
+init()
 
-  /*----- functions -----*/
+function init() {
+    winner = null;
+    score = {
+        x: 0,
+        o: 0,
+        t: 0
+    }
+    currentPlayer = 'x'
+    board = [
+        null, null, null,
+        null, null, null,
+        null, null, null,
+    ]
+    render()
+}
+
+function render(){
+    renderBoard()
+};
+
+function renderBoard() {
+// use the board state and for each cell in the board array
+// either show an X, O, or nothing in each child of the boardEl
+    board.forEach(function(cell) {
+       console.log(cell)
+    })
+};
+
+function handleBoardClick(){
+    console.log('board was clicked')
+};
+
+function handleResetClick(){
+    console.log('reset was clicked')
+};
+
